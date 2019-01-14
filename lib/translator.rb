b5e15659f92252aed247a9ee7735f0fe8e882b97
Dylan Clements <dclements@protonmail.com>
2019-01-14 01:26:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,7 @@ def load_library(yaml_path)
 emoticon_hash = {get_meaning: {}, get_emoticon: {}}
     library = YAML.load_file(yaml_path)
  library.each do |emoticon, keystrokes|
+   binding.pry
     emoticon_hash["get_meaning"][keystrokes[1]] = emoticon
     emoticon_hash["get_emoticon"][keystrokes[0]] = keystrokes[1]
     #binding.pry
