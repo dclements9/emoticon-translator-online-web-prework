6aae96ce03474a11bedfd9fc828dbbee85af817e
Dylan Clements <dclements@protonmail.com>
2019-01-14 01:28:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,9 +8,9 @@ emoticon_hash = {get_meaning: {}, get_emoticon: {}}
    #binding.pry
     emoticon_hash[:get_meaning][keystrokes[1]] = emoticon
     emoticon_hash[:get_emoticon][keystrokes[0]] = keystrokes[1]
-    #binding.pry
+    binding.pry
  end
- binding.pry
+ #binding.pry
   emoticon_hash
 end
 
